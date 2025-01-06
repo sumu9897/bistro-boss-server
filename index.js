46871ca54e297285783e3d0e8780562216fa4612
sumu9897 <sumon.bncd@gmail.com>
2025-01-06 16:15:33 +0600

    Add new collection

--- index.js
+++ index.js
@@ -28,10 +28,23 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("bistroDB").collection("menu")
+    const reviewCollection = client.db("bistroDB").collection("reviews")
+    const cartCollection = client.db("bistroDB").collection("carts")
 
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
         res.send(result)
+    })
+    app.get('/reviews', async(req, res) => {
+        const result = await reviewCollection.find().toArray();
+        res.send(result)
+    })
+
+    // Carts collection
+    app.post('/carts', async(req, res) =>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -52,3 +65,17 @@ app.get('/', (req, res) => {
 app.listen(port , () => {
     console.log(`Bistro Boss is Sitting on port ${port}`)
 })
+
+
+/**
+ * --------------------------------
+ *      NAMING CONVENTION
+ * --------------------------------
+ * app.get('/users')
+ * app.get('/users/:id')
+ * app.post('/users')
+ * app.put('/users/:id')
+ * app.patch('/users/:id')
+ * app.delete('/users/:id')
+ * 
+*/
