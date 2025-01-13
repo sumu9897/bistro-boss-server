56dabf01d71d01eb9a09f337363eb3c8dc0b4548
sumu9897 <sumon.bncd@gmail.com>
2025-01-13 17:16:06 +0600

    Role Change

--- index.js
+++ index.js
@@ -49,6 +49,18 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
+    
+    app.patch('/users/admin/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      } 
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
 
     // user delete 
     app.delete('/users/:id', async (req, res) => {
