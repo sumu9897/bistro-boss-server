56cefb644cf56705254ebbe846951a826ed34e23
sumu9897 <sumon.bncd@gmail.com>
2025-01-13 18:35:35 +0600

    JWT Token set

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -33,11 +34,52 @@ async function run() {
     const reviewCollection = client.db("bistroDB").collection("reviews")
     const cartCollection = client.db("bistroDB").collection("carts")
 
+    // JWT related API
+    app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      })
+      res.send({token});
+    })
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token',req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded= decoded;
+        next();
+      })
+      // next()
+    }
+
     // user related api
-    app.get('/users', async(req, res) => {
+    app.get('/users',verifyToken, async(req, res) => {
+      // console.log(req.headers);
       const result = await userCollection.find().toArray();
       res.send(result)
     })
+
+    app.get('/user/admin/:email',verifyToken, async(req, res) =>{
+      const email = req. params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'unauthorized access'})
+      }
+
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user.role === 'admin';
+      }
+      res.send({admin});
+    } )
     app.post('/users', async (req, res) =>{
       const user = req.body;
 
@@ -49,7 +91,7 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
-    
+
     app.patch('/users/admin/:id', async(req, res) =>{
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
