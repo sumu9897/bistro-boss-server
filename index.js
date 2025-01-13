b7b7ce5067bfc6368725bfe76c0f0f683a4cc5e4
sumu9897 <sumon.bncd@gmail.com>
2025-01-13 09:57:24 +0600

    Add new Collection

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
+
 //middleware
 app.use(cors())
 app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmec6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,9 +28,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("bistroDB").collection("users")
     const menuCollection = client.db("bistroDB").collection("menu")
     const reviewCollection = client.db("bistroDB").collection("reviews")
     const cartCollection = client.db("bistroDB").collection("carts")
+
+    // user related api
+    app.post('/users', async (req, res) =>{
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
 
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
@@ -40,20 +49,31 @@ async function run() {
         res.send(result)
     })
 
+    
+
+    // cart collection read
+    app.get('/carts', async(req,res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
     // Carts collection send
-    app.post('/carts', async(req, res) =>{
+    app.post('/carts', async (req, res) =>{
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     })
 
-    // cart collection read
-    app.get('/carts', async(req,res) => {
-      const email = req.query.email;
-      const query = {email: email}
-      const result = await cartCollection.find(query).toArray();
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
