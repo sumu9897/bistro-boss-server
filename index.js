19d1213c92cf699d45cfeed71589b56011efb60e
sumu9897 <sumon.bncd@gmail.com>
2025-01-13 12:22:49 +0600

    Delete user

--- index.js
+++ index.js
@@ -34,12 +34,31 @@ async function run() {
     const cartCollection = client.db("bistroDB").collection("carts")
 
     // user related api
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
     app.post('/users', async (req, res) =>{
       const user = req.body;
+
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'User already exists', insertedId: null})
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
 
+    // user delete 
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // menu Related Api
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
         res.send(result)
